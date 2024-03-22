229a1f2263c47d2c3052d9f155bd5dfa0da09e99
Cyriac <drevia59@gmail.com>
2024-03-22 11:01:22 +0100

    initial commit

---
+++ src/main/java/com/openclassrooms/medilabofront/MedilabofrontApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.medilabofront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilabofrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilabofrontApplication.class, args);
+	}
+
+}
